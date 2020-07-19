f30e051cbe7b2bceb61077893992a31a2bbf269f
pburgeson <pburgeson@users.noreply.github.com>
2020-07-19 19:56:32 -0400

    Removing IR shutter and updating Readme

--- Software/README.md
+++ Software/README.md
@@ -2,4 +2,5 @@
 2019_06-01_Scanner.ino - Arduino Sketch from openscan.eu
 AccelStepper-1.59.zip - Arduino library for driving a stepper motor
 NewLiquidCrystal_1.5.1.zip - Arduino library for lcd display
+Scanner_Software_No_IRShutter - Arduino Sketch removing IR shutter due to difficulty in finding library
 stepper_drive_step_dir_test - testing of stepper motor drive
--- Software/stepper_drive_accelstepper_test/stepper_drive_accelstepper_test.ino
+++ Software/stepper_drive_accelstepper_test/stepper_drive_accelstepper_test.ino
@@ -1,16 +1,31 @@
 #include <AccelStepper.h>
 
 AccelStepper stepper(1, 6, 7);
-
+const int buttonPin = 3;
+int buttonState = 0;
 void setup() {
-  
-  // put your setup code here, to run once:
+   // set up motor
    stepper.setEnablePin(5);
-   stepper.setMaxSpeed(500);
-   stepper.setSpeed(25);
+   stepper.setMaxSpeed(1600);
+   stepper.setAcceleration(1600);
+   stepper.setPinsInverted(true, false, true);
+   stepper.moveTo(200000);
+
+   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  stepper.runSpeed();
+  // disable outputs is button pressed
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    stepper.disableOutputs();
+  }
+  else {
+    stepper.enableOutputs();
+  }
+  
+  // If at the end of travel go to the other end
+    if (stepper.distanceToGo() == 0)
+      stepper.moveTo(-stepper.currentPosition());
+    stepper.run();
 }
